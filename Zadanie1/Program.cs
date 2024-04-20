b778b5b81314f1909683ee22dd65c2f0541c4fc9
Solidustokarev98 <kirill.tokarev1998@yandex.ru>
2024-04-20 16:32:02 +0500

    Add project files.

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+
+Console.Write("Введите количество строк: ");
+int ROWS = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+        int COLS = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[ROWS, COLS];
+
+
+        Random random = new Random();
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLS; j++)
+            {
+                matrix[i, j] = random.Next(100);
+            }
+        }
+
+        Console.WriteLine("Исходная матрица:");
+        PrintMatrix(matrix);
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+
+        for (int j = 0; j < COLS; j++)
+        {
+            int[] column = new int[COLS];
+            for (int i = 0; i < COLS; i++)
+            {
+                column[i] = matrix[i, j];
+            }
+            Array.Sort(column);
+            for (int i = 0; i < COLS; i++)
+            {
+                matrix[i, j] = column[i];
+            }
+        }
+
+        stopwatch.Stop();
+
+        Console.WriteLine("Отсортированная матрица:");
+        PrintMatrix(matrix);
+
+        Console.WriteLine($"Время сортировки: {stopwatch.Elapsed}");    
+
+static void PrintMatrix(int[,] matrix)
+{
+    int n = matrix.GetLength(0);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
